874ab4fb6d3d443f4a6532b5ac22592b0f7e29ca
Heaust Azure <54670936+Heaust-ops@users.noreply.github.com>
2020-09-29 16:55:02 +0530

    Update cmakeWatch.sh

--- cmakeWatch.sh
+++ cmakeWatch.sh
@@ -14,7 +14,6 @@ t=bin
 # I like to have my build in a dir called bin, if you want any other name, change the value of t to it
 
 while true; do
-sleep 1
 inotifywait -e modify,create,delete -r $x && \
 cd $x && if [ -d "./$t" ]; then rm -Rf ./$t; fi && mkdir $t && cd $t && cmake .. && make && cd $x
 
