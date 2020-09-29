0c038ba60e92a4fedd0b2439bc1b0686fa86e9f7
Heaust Azure <54670936+Heaust-ops@users.noreply.github.com>
2020-09-29 16:51:04 +0530

    Create cmakeWatch.sh
    This is a nodemon like utility for cmake
    Run this in the same directory as your CMakeLists
    It watches for any changes and rebuilds your project

---
+++ cmakeWatch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This is a nodemon like utility for cmake
+# Run this in the same directory as your CMakeLists
+# It watches for any changes and rebuilds your project
+
+# you must have inotify-tools for this to work
+
+# I have this in my home dir and set an alias to it for ease of use
+# like, alias cmakeWatch="~/shellUtilities/cmakeWatch.sh"
+
+x=$(pwd)
+t=bin
+# I like to have my build in a dir called bin, if you want any other name, change the value of t to it
+
+while true; do
+sleep 1
+inotifywait -e modify,create,delete -r $x && \
+cd $x && if [ -d "./$t" ]; then rm -Rf ./$t; fi && mkdir $t && cd $t && cmake .. && make && cd $x
+
+done
